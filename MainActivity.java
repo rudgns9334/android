a3078365241c85eae093523729830687d6d117a3
eun9999 <64730787+eun9999@users.noreply.github.com>
2020-05-03 19:53:54 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,65 +1,95 @@
-package com.example.firebasetest;
+package com.example.lasttest;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.FirebaseError;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static java.sql.Types.NULL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listview;
-    List mat = new ArrayList<>();
-    ArrayAdapter adapter;
-    static boolean calledAlready =false;
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(!calledAlready){
-            FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-            calledAlready = true;
-        }
+        final DBHelper dbHelper = new DBHelper(getApplicationContext(), "Refrigerator.db", null, 1);
 
-        listview = (ListView) findViewById(R.id.list);
+        // 테이블에 있는 모든 데이터 출력
+        final TextView result = (TextView) findViewById(R.id.result);
 
-        adapter = new ArrayAdapter<String>(this, R.layout.activity_list,mat);
-        listview.setAdapter(adapter);
+        final EditText etItem = (EditText) findViewById(R.id.item);
+        final EditText etAmount = (EditText) findViewById(R.id.amount);
 
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference dbRef = db.getReference("material");
-
-        dbRef.child("data").addValueEventListener(new ValueEventListener() {
+        Button insert = (Button) findViewById(R.id.insert);
+        insert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot materialSnapshot : dataSnapshot.getChildren()){
-                    String str = materialSnapshot.child("IRDNT_NM").getValue(String.class);
-                    Log.i("Material name is ", str);
-                    mat.add(str);
+            public void onClick(View v) {
+                String item = etItem.getText().toString();
+                String tempAmount = etAmount.getText().toString();
+                if(tempAmount.equals("")) {
+                    Toast.makeText(MainActivity.this, "양을 입력하세요.", Toast.LENGTH_SHORT).show();
                 }
-                adapter.notifyDataSetChanged();
+                else {
+                    int amount = Integer.parseInt(etAmount.getText().toString());
+                    if (dbHelper.is_equal(item)[0] == 1)
+                        dbHelper.update(item, amount + dbHelper.is_equal(item)[1]);
+                    else
+                        dbHelper.insert(item, amount);
+                    result.setText(dbHelper.getResult());
+                }
             }
+        });
 
+        // DB에 있는 데이터 수정
+        Button update = (Button) findViewById(R.id.update);
+        update.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.w("mat: ","Fail", databaseError.toException());
+            public void onClick(View v) {
+                String item = etItem.getText().toString();
+                int amount = Integer.parseInt(etAmount.getText().toString());
+
+                dbHelper.update(item, amount);
+                result.setText(dbHelper.getResult());
+            }
+        });
+
+        // DB에 있는 데이터 삭제
+        Button delete = (Button) findViewById(R.id.delete);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = etItem.getText().toString();
+                String tempAmount = etAmount.getText().toString();
+
+                if(tempAmount.equals("")) {
+                    dbHelper.delete(item);
+                }else {
+                    int amount = Integer.parseInt(etAmount.getText().toString());
+                    if (dbHelper.is_equal(item)[0] == 1 && dbHelper.is_equal(item)[1] > amount)
+                        dbHelper.update(item, dbHelper.is_equal(item)[1] - amount);
+                    else if(dbHelper.is_equal(item)[1] < amount)
+                        Toast.makeText(MainActivity.this, "수량 초과입니다.", Toast.LENGTH_SHORT).show();
+
+                }
+                result.setText(dbHelper.getResult());
+            }
+        });
+
+        // DB에 있는 데이터 조회
+        Button select = (Button) findViewById(R.id.select);
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result.setText(dbHelper.getResult());
             }
         });
     }
