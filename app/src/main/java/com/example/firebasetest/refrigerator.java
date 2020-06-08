7eec64a0f3af5ff5464c56d05fccf182606b8956
eun <dksdmsrud9@gmail.com>
2020-06-09 00:27:27 +0900

    layout

--- app/src/main/java/com/example/firebasetest/refrigerator.java
+++ app/src/main/java/com/example/firebasetest/refrigerator.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class refrigerator extends AppCompatActivity {
 
@@ -19,6 +21,7 @@ public class refrigerator extends AppCompatActivity {
         setContentView(R.layout.refrigerator);
 
         ActionBar ab = getSupportActionBar();
+
         ImageButton imageButton = (ImageButton) findViewById(R.id.refrigerator);
         imageButton.setOnClickListener(new View.OnClickListener() {
 
@@ -28,7 +31,6 @@ public class refrigerator extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
+        getSupportActionBar().setTitle("요리킹 조리킹 !");
     }
 }
